e4a9232c5f4f5757c15e43b2ce981d56abf8a5e2
Vivatist <vivatist@users.noreply.github.com>
2025-11-23 18:51:42 +0300

    Удалены файлы и модули, связанные с обработкой статусов файлов

--- app/flows/file_status_processor/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-File Status Processor Flow - обработка изменений файлов
-"""
-
-from .database import Database
-from .service import FileStatusProcessorService
-
-__all__ = ['Database', 'FileStatusProcessorService']
